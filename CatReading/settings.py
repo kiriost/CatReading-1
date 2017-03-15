e25c68616bf48c328ce2d621c2a1273246b91430
mcczj <616254086@qq.com>
2017-03-15 20:30:26 +0800

    添加数据库和静态文件的设置

--- CatReading/settings.py
+++ CatReading/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from .custom_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -55,7 +56,8 @@ ROOT_URLCONF = 'CatReading.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 模板目录地址设置
+        'DIRS':  [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
