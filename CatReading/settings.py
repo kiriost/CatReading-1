dc0ebcd0bbf46f3a868fe607ee6d4b6bc0c3967c
mcczj <616254086@qq.com>
2017-03-15 18:57:19 +0800

    设置mysql数据库

--- CatReading/settings.py
+++ CatReading/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'CatReading.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CatReading',
+        'USER': 'root',
+        'PASSWORD': 'Mcc616254086',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
